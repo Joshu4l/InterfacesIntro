e1efb33228b5cc36811149f107bf6a8aefad0ab5
joshuaalbert <albertjoshua5@gmail.com>
2023-08-31 11:02:26 +0200

    completed steps 1 to 5 of the group task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,35 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+/*
+Erstellt euch ein neues Java-Projekt in IntelliJ (bitte nur eine Backend-Anwendung) und implementiert folgende Schritte:
+
+Schritt 1: Definiert ein Interface 'Playable', das eine Methode 'play' ohne Implementierung enthält.
+Schritt 2: Erstellt eine Klasse 'MusicPlayer', die das 'Playable'-Interface implementiert.
+           Implementiert die 'play'-Methode, um einen Song abzuspielen.
+Schritt 3: Erstellt eine Klasse 'VideoPlayer', die ebenfalls das 'Playable'-Interface implementiert.
+           Implementiert die 'play'-Methode, um ein Video abzuspielen.
+Schritt 4: Erstellt eine Klasse 'MediaController', die eine Methode 'playMedia' enthält.
+           Diese Methode soll ein 'Playable'-Objekt als Parameter akzeptieren und die 'play'-Methode aufrufen.
+Schritt 5.1: In der 'main'-Methode erstellt ein Objekt der Klasse 'MediaController' und nutzt es,
+             um sowohl einen Song als auch ein Video abzuspielen.
+Schritt 5.2: In der 'main'-Methode initialisiert jeweils ein Objekt der Klasse 'MusicPlayer' und
+             'VideoPlayer' mit dem Interface 'Playable' (Polymorphismus).
+ */
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Instantiate a MediaController
+        MediaController myController = new MediaController();
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        // Make the MediaController play a Song
+        myController.playMedia(new MusicPlayer());
+        // Make the MediaController play a Video
+        myController.playMedia(new VideoPlayer());
+
+        // Polymorphism example
+        Playable myMusicPlayer = new MusicPlayer();
+        myController.playMedia(myMusicPlayer);
+
     }
 }
